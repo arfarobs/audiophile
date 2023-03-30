38053b3c9a4f5a0b9355110833134cd37687e5cb
arfarobs <arthurrobertswork@outlook.com>
2023-03-30 11:42:49 +0800

    Footer component tests wrote.

--- src/components/common/category-heading/CategoryHeading.test.js
+++ src/components/common/category-heading/CategoryHeading.test.js
@@ -4,7 +4,7 @@ import CategoryHeading from './CategoryHeading';
 describe('CategoryHeading', () => {
 	it('renders the category heading with the correct text', () => {
 		render(<CategoryHeading category="Ohh! Look at me!!" />);
-		const heading = screen.getByText("Ohh! Look at me!!");
+		const heading = screen.getByText('Ohh! Look at me!!');
 		expect(heading).toBeInTheDocument();
 	});
-})
+});
