8dab71ae26fb8e1f639c85cbfaaff054864aa51e
arfarobs <arthurrobertswork@outlook.com>
2023-04-03 18:11:13 +0800

    RouteLayout testing complete

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -14,16 +14,18 @@ import NotFound from '../components/common/not-found/NotFound';
 //   Layouts
 import RouteLayout from '../components/layouts/RouteLayout';
 
+export const routerConfig = createRoutesFromElements(
+	<Route path="/" element={<RouteLayout />}>
+		<Route index element={<Home />} />
+		<Route path="category/:category" element={<Category />} />
+		<Route path="category/:category/product-details/:product" element={<Product />} />
+		<Route path="checkout" element={<Checkout />} />
+		<Route path="*" element={<NotFound />} />
+	</Route>
+);
+
 const router = createBrowserRouter(
-	createRoutesFromElements(
-		<Route path="/" element={<RouteLayout />}>
-			<Route index element={<Home />} />
-			<Route path="category/:category" element={<Category />} />
-			<Route path="category/:category/product-details/:product" element={<Product />} />
-			<Route path="checkout" element={<Checkout />} />
-			<Route path="*" element={<NotFound />} />
-		</Route>
-	)
+	routerConfig,
 );
 
 export default router;
