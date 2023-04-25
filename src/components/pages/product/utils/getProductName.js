7a261ae16480119d981689376374e95596b736c6
arfarobs <arthurrobertswork@outlook.com>
2023-04-25 14:42:56 +0800

    getProductName test complete

--- src/components/pages/product/utils/getProductName.js
+++ src/components/pages/product/utils/getProductName.js
@@ -1,8 +1,9 @@
 export const getProductName = (name) => {
 	let productName = name.split(' ').slice(0, -1).join(' ');
 	
-	if (productName.includes('Mark')) {
-		productName = productName.replace('Mark', 'MK');
+	if (productName.toLowerCase().includes('mark')) {
+		productName = productName.replace(/mark/gi, 'MK');
 	}
+	
 	return productName;
 };
