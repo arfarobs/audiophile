60233bc19140289d0ef844d2d899e2edff912ff2
arfarobs <arthurrobertswork@outlook.com>
2023-05-09 18:44:39 +0800

    fixed linting errors

--- src/store/productSlice.test.js
+++ src/store/productSlice.test.js
@@ -12,26 +12,26 @@ describe('productSlice', () => {
 		store.dispatch(resetQuantity());
 	});
 
-  it('should handle incrementing product quantity', () => {
-    store.dispatch(handleProductQuantity('up'));
-    expect(store.getState().product.productQuantity).toEqual(2);
-  });
+	it('should handle incrementing product quantity', () => {
+		store.dispatch(handleProductQuantity('up'));
+		expect(store.getState().product.productQuantity).toEqual(2);
+	});
 
-  it('should handle decrementing product quantity', () => {
-    store.dispatch(handleProductQuantity('up'));
-    store.dispatch(handleProductQuantity('up'));
-    store.dispatch(handleProductQuantity('down'));
-    expect(store.getState().product.productQuantity).toEqual(2);
-  });
+	it('should handle decrementing product quantity', () => {
+		store.dispatch(handleProductQuantity('up'));
+		store.dispatch(handleProductQuantity('up'));
+		store.dispatch(handleProductQuantity('down'));
+		expect(store.getState().product.productQuantity).toEqual(2);
+	});
 
-  it('should not decrement product quantity below 1', () => {
-    store.dispatch(handleProductQuantity('down'));
-    expect(store.getState().product.productQuantity).toEqual(1);
-  });
+	it('should not decrement product quantity below 1', () => {
+		store.dispatch(handleProductQuantity('down'));
+		expect(store.getState().product.productQuantity).toEqual(1);
+	});
 
-  it('should reset product quantity to 1', () => {
-    store.dispatch(handleProductQuantity('up'));
-    store.dispatch(resetQuantity());
-    expect(store.getState().product.productQuantity).toEqual(1);
-  });
+	it('should reset product quantity to 1', () => {
+		store.dispatch(handleProductQuantity('up'));
+		store.dispatch(resetQuantity());
+		expect(store.getState().product.productQuantity).toEqual(1);
+	});
 });
